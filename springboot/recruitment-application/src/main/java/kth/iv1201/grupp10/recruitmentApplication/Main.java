e58913ffe47f1d395b217992f4ebea2e0be428e4
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-02-06 13:05:56 +0100

    Change to war packaging, Add Tomcat configuration

--- springboot/recruitment-application/src/main/java/kth/iv1201/grupp10/recruitmentApplication/Main.java
+++ springboot/recruitment-application/src/main/java/kth/iv1201/grupp10/recruitmentApplication/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer{
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
