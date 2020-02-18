07411cbd5c870418da02365367e9bee0ff3aa419
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-02-18 13:56:47 +0100

    Merge sia/feat/jwt into master

--- angular/recruitment-application/src/app/app-routing.module.ts
+++ angular/recruitment-application/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ApplicationListComponent } from './components/application-list/application-list.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'appList', component: ApplicationListComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- angular/recruitment-application/src/app/components/login/login.component.ts
+++ angular/recruitment-application/src/app/components/login/login.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from '../../app.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   credentials = {email: '', password: ''};
 
-  constructor(private app: AppService, private http: HttpClient, private router: Router) {}
-
-  ngOnInit() {
-  }
+  constructor(private http: HttpClient, private router: Router, private auth: AuthService) {}
 
   login() {
-    this.app.authenticate(this.credentials, () => {
-        this.router.navigateByUrl('');
-    });
-    return false;
+    this.auth.login(this.credentials)
+      .subscribe(
+        res => {
+          console.log(res);
+          localStorage.setItem('token', res['token']);
+          this.router.navigateByUrl('');
+        },
+        err => {console.log(err); console.log('hej'); }
+      );
   }
 }
