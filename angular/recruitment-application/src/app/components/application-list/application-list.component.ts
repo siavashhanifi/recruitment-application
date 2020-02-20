de269a3265120fbbd7149aff0f611311fbd684b0
sebastiandomarbolmstam <bolmstam@kth.se>
2020-02-20 13:39:16 +0100

    Comments in application-list.ts

--- angular/recruitment-application/src/app/components/application-list/application-list.component.ts
+++ angular/recruitment-application/src/app/components/application-list/application-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Component decorator containing selector, template url and style urls.
+ * The selector is used for identifying the component in html.
+ * The template url defines the html associated to the component.
+ * The sytle url defines the stylesheets related to the component.
+ */
 @Component({
   selector: 'app-application-list',
   templateUrl: './application-list.component.html',
