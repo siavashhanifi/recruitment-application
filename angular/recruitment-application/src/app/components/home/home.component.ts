12be4958d3f9ee37e5722d1776eafdb01d5d5476
sebastiandomarbolmstam <bolmstam@kth.se>
2020-02-20 13:40:05 +0100

    Comments in home.ts

--- angular/recruitment-application/src/app/components/home/home.component.ts
+++ angular/recruitment-application/src/app/components/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Component decorator containing selector, template url and style urls.
+ * The selector is used for identifying the component in html.
+ * The template url defines the html associated to the component.
+ * The sytle url defines the stylesheets related to the component.
+ */
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
