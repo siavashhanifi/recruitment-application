d82d49cdf98a1d3b9f5f8a00594c0adbb5303862
sebastiandomarbolmstam <bolmstam@kth.se>
2020-02-20 14:25:44 +0100

    Comments in app.component.ts

--- angular/recruitment-application/src/app/app.component.ts
+++ angular/recruitment-application/src/app/app.component.ts
@@ -4,6 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/finally';
 
+/**
+ * Component decorator containing selector, template url and style urls.
+ * The selector is used for identifying the component in html.
+ * The template url defines the html associated to the component.
+ * The sytle url defines the stylesheets related to the component.
+ * @export
+ * @class AppComponent with application title
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
