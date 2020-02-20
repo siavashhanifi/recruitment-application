8ae41cc8116fec44e49b3e74b6a4dfd185eef340
sebastiandomarbolmstam <bolmstam@kth.se>
2020-02-20 13:37:15 +0100

    Comments in login.ts

--- angular/recruitment-application/src/app/components/login/login.component.ts
+++ angular/recruitment-application/src/app/components/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth.service';
 /**
- * Component
+ * Component decorator containing selector, template url and style urls.
+ * The selector is used for identifying the component in html.
+ * The template url defines the html associated to the component.
+ * The sytle url defines the stylesheets related to the component.
  */
 @Component({
   selector: 'app-login',
@@ -14,19 +17,33 @@ import { AuthService } from '../../auth.service';
 
 export class LoginComponent {
 
-  credentials = {email: '', password: ''};
+/**
+ * Credentials  of login component. Variables containing user input from a login-attempt.
+ */
+credentials = {email: '', password: ''};
 
-  constructor(private http: HttpClient, private router: Router, private auth: AuthService) {}
+/**
+ * Creates an instance of login component.
+ * @param http instance of HttpClient responsible for http communication with backend server
+ * @param router instance of frontend router for page navigation
+ * @param auth  instance of a service respinsible for backend server communication ralated to authentication
+ */
+constructor(private http: HttpClient, private router: Router, private auth: AuthService) {}
 
-  login() {
-    this.auth.login(this.credentials)
-      .subscribe(
-        res => {
-          console.log(res);
-          localStorage.setItem('token', res['token']);
-          this.router.navigateByUrl('');
-        },
-        err => {console.log(err); window.alert(err.error.message) }
-      );
+/**
+ * Method called at a login attempt. Calls the login handler in authservice (@see ../../auth.service#login())
+ * and recieves an observable. If a valid response is returned, the token passed from backend is stored
+ * in the local storage (the user is logged in) and redirects to the main page.
+ */
+login() {
+  this.auth.login(this.credentials)
+    .subscribe(
+      res => {
+        console.log(res);
+        localStorage.setItem('token', res['token']);
+        this.router.navigateByUrl('');
+      },
+      err => {console.log(err); window.alert(err.error.message); }
+    );
   }
 }
