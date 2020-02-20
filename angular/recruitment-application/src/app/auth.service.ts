81627a4e179756023a273fbc9aa04359f53ef61c
sebastiandomarbolmstam <bolmstam@kth.se>
2020-02-20 15:02:08 +0100

    Comments in auth-service.ts

--- angular/recruitment-application/src/app/auth.service.ts
+++ angular/recruitment-application/src/app/auth.service.ts
@@ -9,7 +9,6 @@ import { map } from 'rxjs/operators';
  * Class AuthService responsible for backend communication related to user authorization.
  *
  * @export
- * @class AuthService
  */
 @Injectable({
   providedIn: 'root'
@@ -21,30 +20,62 @@ export class AuthService {
    * registrationURL: url for backend registration handling
    * loginURL: url for backend login handling
    * validURL: url for backend token validation
-   *
-   * @private
-   * @memberof AuthService
    */
   private registerURL = 'api/auth/register';
   private loginURL = 'api/auth/login';
   private validURL = 'api/auth/validToken';
 
+
+  /**
+   * Creates an instance of AuthService.
+   * @param http instance of HttpClient responsible for http communication with backend server
+   * @param router instance of frontend router for page navigation
+   */
   constructor(private http: HttpClient, private router: Router, ) {
   }
 
+
+  /**
+   * register method sends post-request to backend registration handling, passing the new user object
+   * as argument
+   *
+   * @param user
+   * @returns observable response of backend register process.
+   */
   register(user: any) {
 
     return this.http.post(this.registerURL, user);
   }
 
+
+  /**
+   * login method sends post-request to backend for login handling, passing the login-cridentials
+   * as argument. JWT-token included in response header if login was successful.
+   *
+   * @param user
+   * @returns observable response of backend login process
+   */
   login(user: any) {
     return this.http.post(this.loginURL, user);
   }
 
+
+  /**
+   * Method for retrieving JWT-token from local storage.
+   *
+   * @returns JWT-token
+   */
   getToken() {
     return localStorage.getItem('token');
   }
 
+
+  /**
+   * Method for validation of current token stored in local storage through a get-request
+   * to the backend server
+   *
+   * @returns observable returning true if token is valid.
+   */
   isValid(): Observable<boolean> {
     let valid: Observable<boolean>;
 
@@ -59,34 +90,35 @@ export class AuthService {
     );
     console.log(valid);
     return valid;
-    /*this.http.get(this.validURL).subscribe(
-      res => {
-        if (res['validToken']) {
-          if (res['role'] === '2') {
-            return true;
-          } else {return false; }
-        } else {
-          return false;
-        }
-      },
-      err => {
-        return false;
-        console.log(err);
-      }
-    );*/
   }
 
+
+  /**
+   * Method for checking if the user is logged in, only affecting fronend.
+   *
+   * @returns boolean true or false, if a JWT-token is found or not.
+   */
   loggedIn() {
     return !!localStorage.getItem('token');
   }
 
+
+  /**
+   * Method for removing the JWT-token form local storage, equvivalent to logging out.
+   *
+   */
   logout() {
     localStorage.removeItem('token');
     this.router.navigateByUrl('/');
   }
 
+
+  /**
+   * Method for controlling if a user is of the correct authorization level to access specific pages.
+   *
+   * @returns boolean, authorized or not.
+   */
   isAuthorized() {
-    console.log('here');
     const token = jwt_decode(localStorage.getItem('token'));
     console.log(token);
     if (token['role_id'] === 2) {
