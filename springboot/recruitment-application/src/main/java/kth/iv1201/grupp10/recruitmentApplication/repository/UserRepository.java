6e4e9756014a155f60bfab0f171569fa43affe0c
Siavash Hanifi <siavash.hanifi@gmail.com>
2020-02-21 13:53:12 +0100

    Remove unused import from RequestController

--- springboot/recruitment-application/src/main/java/kth/iv1201/grupp10/recruitmentApplication/repository/UserRepository.java
+++ springboot/recruitment-application/src/main/java/kth/iv1201/grupp10/recruitmentApplication/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package kth.iv1201.grupp10.recruitmentApplication.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
